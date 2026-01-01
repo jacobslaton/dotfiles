369a3f62155b0f913298e6a34d96a8bbb2369200
Jacob Slaton <jacobslaton@protonmail.com>
2026-01-01 16:16:48 -0700

    Cleanup Color Scheme and Configure Font

--- dotfiles/linux/.wezterm.lua
+++ dotfiles/linux/.wezterm.lua
@@ -1,27 +1,12 @@
 local wezterm = require 'wezterm'
 config = wezterm.config_builder()
 
---config.color_scheme = 'Banana Blueberry' --Freya --Griffin
---config.color_scheme = 'Belge (terminal.sexy)' --Freya
---config.color_scheme = 'Bim (Gogh)' --Freya --Griffin
---config.color_scheme = 'Blue Matrix' --Freya
---config.color_scheme = 'BlueBerryPie' --Freya
---config.color_scheme = 'Breath Darker (Gogh)' --Freya
---config.color_scheme = 'Brewer (base16)' --Freya --Griffin
---config.color_scheme = 'Cai (Gogh)' --Freya --Griffin
-config.color_scheme = 'carbonfox' --Freya
-
---config.color_scheme = 'Atelier Lakeside (base16)'
---config.color_scheme = 'Atelier Seaside (base16)'
---config.color_scheme = 'Bright (base16)'
---config.color_scheme = 'Canvased Pastel (terminal.sexy)'
---config.color_scheme = 'Cloud (terminal.sexy)'
---config.color_scheme = 'Cyberdyne'
---config.color_scheme = 'cyberpunk'
---config.color_scheme = 'Sakura'
---config.color_scheme = 'Slate'
+config.color_scheme = 'carbonfox'
 
 config.window_background_opacity = 0.8
+
+config.font_size = 10
+config.font = wezterm.font 'Mononoki Nerd Font Mono'
 
 return config
 
